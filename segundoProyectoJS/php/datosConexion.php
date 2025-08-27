e4d10178739da0a5b2eba3586fa8b873a7ee7b76
Jonathan04102006 <jonaperez0406@gmail.com>
2025-08-26 21:23:18 -0700

    Correciones

--- segundoProyectoJS/php/datosConexion.php
+++ segundoProyectoJS/php/datosConexion.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "albms";
+$database = "albums";
 
 $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
