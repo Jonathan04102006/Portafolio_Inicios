29e3adfbcf0bf1035efd20a279efe5352fe5f73e
Jonathan04102006 <jonaperez0406@gmail.com>
2025-10-15 22:54:19 -0700

    POO

--- JS-POO/polimorfismo.js
+++ JS-POO/polimorfismo.js
@@ -22,9 +22,47 @@ class Guitarra extends Instrumento{
     }
 }
 
-console.log("\x1b[31m%s\x1b[0m", "La ${this.nombre} Este texto es rojo")
-console.log("\x1b[31m%s\x1b[0m", "La ${this.nombre} Este texto es verde")
-console.log("\x1b[31m%s\x1b[0m", "La ${this.nombre} Este texto es amarillo")
-console.log("\x1b[31m%s\x1b[0m", "La ${this.nombre} Este texto es azul")
-console.log("\x1b[31m%s\x1b[0m", "La ${this.nombre} Este texto es magenta")
-console.log("\x1b[31m%s\x1b[0m", "La ${this.nombre} Este texto es cian")
+class Bateria extends Instrumento{
+    constructor(){
+        super('Batería')
+    }
+
+    // POLIMORFISMO: es la capacidad de múltiples clases a responder distinto a una misma función o método
+
+    // Sobreescribir o pisar [OVERRIDE] el método heredado
+    tocar(){
+        console.log("\x1b[32m%s\x1b[0m", `La ${this.nombre} está tocando los tambores a ungran ritmo!`)
+    }
+}
+
+class Piano extends Instrumento{
+    constructor(){
+        super('Piano')
+    }
+
+    // POLIMORFISMO: es la capacidad de múltiples clases a responder distinto a una misma función o método
+
+    // Sobreescribir o pisar [OVERRIDE] el método heredado
+    tocar(){
+        console.log("\x1b[34m%s\x1b[0m", `El ${this.nombre} está haciendo unas hermosas notas pentatónicas de blues!`)
+    }
+}
+
+function showBanda(instrumento){
+    instrumento.tocar()
+}
+
+const guitarra = new Guitarra()
+const bateria = new Bateria()
+const piano = new Piano()
+
+showBanda(guitarra)
+showBanda(bateria)
+showBanda(piano)
+
+// console.log("\x1b[31m%s\x1b[0m", "La ${this.nombre} Este texto es rojo")
+// console.log("\x1b[31m%s\x1b[0m", "La ${this.nombre} Este texto es verde")
+// console.log("\x1b[31m%s\x1b[0m", "La ${this.nombre} Este texto es amarillo")
+// console.log("\x1b[31m%s\x1b[0m", "La ${this.nombre} Este texto es azul")
+// console.log("\x1b[31m%s\x1b[0m", "La ${this.nombre} Este texto es magenta")
+// console.log("\x1b[31m%s\x1b[0m", "La ${this.nombre} Este texto es cian")
