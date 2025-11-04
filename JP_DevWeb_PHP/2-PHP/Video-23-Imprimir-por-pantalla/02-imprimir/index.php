913dd63d4b9957ad69a2771a2c894f49a4e41f03
Jonathan04102006 <jonaperez0406@gmail.com>
2025-11-03 22:39:50 -0800

    PHP

---
+++ JP_DevWeb_PHP/2-PHP/Video-23-Imprimir-por-pantalla/02-imprimir/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPR HTML><html>
+    <head>
+        <meta charset="uft-8"/>
+        <title>Imprimir por pantalla - Master en PHP</title>
+    </head>
+    <body>
+        
+        <h1>Master en PHP - <?php echo "jonaperez0406@gmail.com"; ?> </h1>
+        
+        <?= "Bienvenido al curso más grande de PHP" ?>
+        
+        <?php
+            echo "<h3>Listado de videojuegos:</h3>";
+            echo "<ul>"
+                    . "<li>GTA</li>"
+                    . "<li>Fifa</li>"
+                    . "<li>Mario Bros</li>"
+                    ."</ul>";
+            
+            echo '<p>Esta es toda'.'-'.'lista de juegos</p>';
+        ?>
+        
+    </body>
+</html>
