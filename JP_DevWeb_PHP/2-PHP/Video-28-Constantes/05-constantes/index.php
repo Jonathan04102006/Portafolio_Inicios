cc295349eed085cce906504724c2ef6eb87a6366
Jonathan04102006 <jonaperez0406@gmail.com>
2025-11-04 22:44:42 -0800

    PHP_constantes(28)

---
+++ JP_DevWeb_PHP/2-PHP/Video-28-Constantes/05-constantes/index.php
@@ -0,0 +1,18 @@
+<?php
+
+// Constantes
+// Es un contenedor de información que no puede variar
+
+define('nombre', 'Jonathan Pérez');
+define ('web', 'jl958329@gmail.com');
+
+echo '<h1>'.nombre.'</h1>';
+echo '<br>';
+echo '<h1>'.web.'</h1>';
+
+// Variable
+$web = "jonapere0406@gmail.com";
+echo '<h1>'.$web.'</h1>';
+
+$web = "jl958329@gmail.com";
+echo '<h1>'.$web.'</h1>';
