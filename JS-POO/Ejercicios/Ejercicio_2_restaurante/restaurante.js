d615cb464507bf47dac5beb201509c97562790d9
Jonathan04102006 <jonaperez0406@gmail.com>
2025-11-10 13:49:41 -0800

    POO_ejercicio

--- JS-POO/Ejercicios/Ejercicio_2_restaurante/restaurante.js
+++ JS-POO/Ejercicios/Ejercicio_2_restaurante/restaurante.js
@@ -1,20 +1,43 @@
-// Clase Menú
-class Menu {
-    // Recibira un array de opciones (["Pizza", "Hamburguesa", "Ensalada"])
-    constructor(opciones) {
-        this._opciones = opciones;
+// Clase que representa al Restaurante
+class Restaurante {
+    constructor() {
+        this.comidas = ["Pizza", "Hamburguesa", "Ensalada"];
+        this.bebidas = ["Agua", "Refresco", "Cafe"];
+    }
+}
+
+// Clase que maneja la Interfaz de Usuario
+class InterfazUsuario {
+    constructor () {
+        this.restaurante = new Restaurante();
+        this.formulario = document.getElementById('formulario');
+        this.agregarMasEl = document.getElementById('agregar-mas')
+    }
+    
+    // Método privado (enlaza eventos)
+    _iniciarEventos(){
+        document.getElementById('btn-pedir').addEventListener('click', () => this._confirmarPedido())
+        document.getElementById('btn-si').addEventListener('click', () => this._reiniciar())
+        document.getElementById('btn-no').addEventListener('click', () => this._finalizar())
+    }
+    
+    // Confirmar pedido
+    _confirmarPedido(){
+        const platillo = this.selectComida.value;
+        const bebida = this.selectBebida.value;
+
+        const mensaje = this.restaurante.generarMensaje(platillo, bebida);
+        this._mostrarMensaje(mensaje, "exito")
+        this.formulario.classList.add('oculto')
+        this.agregarMasEl.classList.remove('oculto')
     }
 
-    mostrar() {
-        this._opciones.forEach((opcion, index) => {
-            console.log(`${index + 1}. ${opcion}`);
-        });
+    _reiniciar(){
+
     }
 
-    obtenerOpcion(numero) {
-        const idx = parseInt(numero, 10) - 1;
-        if (idx >= 0 && idx < this._opciones.length) {
-            return this._opciones[idx];
-        }
+    _finalizar(){
+
     }
+
 }
