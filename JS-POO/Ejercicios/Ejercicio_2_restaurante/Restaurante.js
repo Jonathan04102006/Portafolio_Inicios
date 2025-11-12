07d7d076e906e32dc389b9d64bc55e6d639997ef
Jonathan04102006 <jonaperez0406@gmail.com>
2025-11-12 11:35:18 -0800

    Restaurante_POO

---
+++ JS-POO/Ejercicios/Ejercicio_2_restaurante/Restaurante.js
@@ -0,0 +1,9 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+// Programa de toma de pedidos para un restaurante
+// Menú: Pizza, Hamburguesa, Ensalada
+// Bebidas: Agua, Refresco, Cafe
