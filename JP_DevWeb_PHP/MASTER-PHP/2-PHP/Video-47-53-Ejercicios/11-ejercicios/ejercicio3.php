68e2c3b672dbb04920c363c026dbe0b84d89f281
Jonathan04102006 <jonaperez0406@gmail.com>
2025-12-17 20:01:59 -0800

    ejercicios de PHP

--- JP_DevWeb_PHP/MASTER-PHP/2-PHP/Video-47-53-Ejercicios/11-ejercicios/ejercicio3.php
+++ JP_DevWeb_PHP/MASTER-PHP/2-PHP/Video-47-53-Ejercicios/11-ejercicios/ejercicio3.php
@@ -6,10 +6,17 @@
  * PD: Utilizando bucle while.
  */
 
-$contador = 0;
+/*
+ * $contador = 0;
 while($contador <= 40){
     $cuadrado = $contador*$contador;
     echo "<h3>El cuadrado de $contador es $cuadrado</h3>";
     
     $contador++;
+}
+ */
+
+for($contador = 0; $contador <= 40; $contador++){
+    $cuadrado = $contador*$contador;
+    echo "<h3>El cuadrado de $contador es $cuadrado</h3>";
 }
