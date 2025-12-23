9f5abcab685f7677575485d0d3c2bdc949754d88
Jonathan04102006 <jonaperez0406@gmail.com>
2025-12-22 17:43:41 -0800

    JS

--- JS-POO/VIDEOS/3_video_CLASES_PROPIEDADES/index.js
+++ JS-POO/VIDEOS/3_video_CLASES_PROPIEDADES/index.js
@@ -1,2 +1,3 @@
-// url de 3_video_CLASES_(PROPIEDADES) = C:\Proyectos\Portafolio\Portafolio_Inicios\JS-POO\VIDEOS
+// url de 3_video_CLASES_(PROPIEDADES) = 
+// node C:\Proyectos\Portafolio\Portafolio_Inicios\JS-POO\VIDEOS\3_video_CLASES_PROPIEDADES\index.js
 console.log('Hola');
