867f1b53ba827c3d8de3c3d58360073a872a2f1d
Jonathan04102006 <jonaperez0406@gmail.com>
2026-01-12 22:49:37 -0800

    Video 9 de JS_POO

---
+++ JS-POO/Curso_de_POO_jonmircha/Video_9_undefined_,_null_&_NaN/index.js
@@ -0,0 +1,11 @@
+// Undefined indica que no se ha inicializado una variable y que el valor esta ausente.
+let indefinida;
+console.log(indefinida);
+
+// null es un valor especial que indica la ausencia de un valor.
+let nulo = null;
+console.log(null);
+
+// NaN - Not a Number
+let noEsUnNombre = "hola" * 3.7;
+console.log(noEsUnNombre);
