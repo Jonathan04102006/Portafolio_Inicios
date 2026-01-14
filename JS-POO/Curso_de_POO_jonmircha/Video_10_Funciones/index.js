49aed2c6e9c2bd8e27f603a3b09a46f5fd04080c
Jonathan04102006 <jonaperez0406@gmail.com>
2026-01-13 18:41:04 -0800

    Video 10 de JS_POO

--- JS-POO/Curso_de_POO_jonmircha/Video_10_Funciones/index.js
+++ JS-POO/Curso_de_POO_jonmircha/Video_10_Funciones/index.js
@@ -6,8 +6,7 @@ Las funciones en JavaScript son objetos, un tipo especial de objetos:
 Se dice que las funciones son ciudadanos de primera clase porque pueden asignarse a un valor, y pueden pasarse como argumentos y usarse como valor de retorno.
 */
 
-// Funcion Declarada
-
+// Declaracion de funcion
 function estoEsUnaFuncion(){
     console.log("Uno");
     console.log("De");
@@ -28,6 +27,32 @@ function unaFuncionQueDevuelveValor(){
     return "La funcion ha retornado una Cadena de texto";
 }
 
-let valorDeFuncion = unaFuncionQueDevuelveValor();
+// let valorDeFuncion = unaFuncionQueDevuelveValor();
 
-console.log(valorDeFuncion);
+// console.log(valorDeFuncion);
+
+function saludar(nombre = "Desconocido", edad = 0){
+    console.log(`Hola mi nombre es ${nombre} y tengo ${edad} años.`);
+}
+
+// saludar("Ariel", 19);
+// saludar();
+
+// Funciones declaradas VS funciones expresadas
+
+funcionDeclarada();
+
+function funcionDeclarada(){
+    console.log(`Esto es una funcion declarada, puede invocarse en cualquier parte de nuestro codigo, incluso antes de que la funcion sea declarada`);
+}
+
+funcionDeclarada();
+
+// funcionExpresada();
+
+// Funcion anonima
+const funcionExpresada = function(){
+    console.log(`Esto es una funcion expresada, es decir, una funcion que sea ha asignado como valor a una vaariable, si invocamos esta funcion antes de su definicion JS nos dira 'Cannot access 'funcionExpresada' before initialization'`)
+}
+
+funcionExpresada();
