d4d148062db6f9e327e1853546897af4ee95a32d
Jonathan04102006 <jonaperez0406@gmail.com>
2026-01-15 20:29:03 -0800

    ejercicios POO

---
+++ JS-POO/Curso_de_POO_jonmircha/Video_7_Numeros_(Numbers)/ejercicio/index.js
@@ -0,0 +1,7 @@
+/* Conversión y suma: Declara una variable numérica con decimales y otra variable tipo string que represente un número. Convierte el string a número y muestra en consola la suma correcta de ambos valores.
+*/
+console.log("************************ejercicio 1 Numbers************************");
+var numero = 1.78;
+var numero1 = "5";
+console.log(parseFloat(numero1));
+console.log(numero + parseFloat(numero1));
