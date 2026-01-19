8cf4b52be190253753a0eb0bd8b8eeb3dfdbfd35
Jonathan04102006 <jonaperez0406@gmail.com>
2026-01-19 09:14:46 -0800

    ejercicios

--- JS-POO/Curso_de_POO_jonmircha/Video_7_Numeros_(Numbers)/ejercicio/index.js
+++ JS-POO/Curso_de_POO_jonmircha/Video_7_Numeros_(Numbers)/ejercicio/index.js
@@ -6,5 +6,18 @@ var numero1 = "5";
 console.log(parseFloat(numero1));
 console.log(numero + parseFloat(numero1));
 
-/* Conversión y suma: Declara una variable numérica con decimales y otra variable tipo string que represente un número. Convierte el string a número y muestra en consola la suma correcta de ambos valores.
+/* Redondeo de números: Declara un número con varios decimales y muestra en consola: El número redondeado a 2 decimales El número redondeado a 4 decimales
 */
+console.log("************************ejercicio 2 Numbers************************");
+let a = 7.9678967;
+let b = 4.9888676;
+console.log(a.toFixed(2));
+console.log(b.toFixed(4));
+
+/* Tipos de datos y operaciones: Declara una variable number y otra string que contenga un número. Muestra en consola: El tipo de dato de cada variable El resultado de sumarlas sin conversión El resultado de sumarlas convirtiendo el string a número
+*/
+console.log("************************ejercicio 3 Numbers************************");
+var c = 50;
+var d = "75";
+console.log(c + d);
+console.log(c + parseFloat(d));
---
+++ JS-POO/Curso_de_POO_jonmircha/Video_8_Booleans/ejercicio/index.js
@@ -0,0 +1,31 @@
+/* Declara dos variables booleanas y muestra en consola su valor y su tipo de dato usando typeof.
+*/
+console.log("*******************ejercicio 1 de booleans*******************");
+let verdadero = true;
+let falso = false;
+console.log(verdadero, falso);
+console.log(typeof verdadero, typeof falso);
+
+/* Convierte los siguientes valores a booleano y muestra el resultado en consola: 0, 1, "", "Hola", null.
+*/
+console.log("*******************ejercicio 2 de booleans*******************");
+let a = 0;
+let b = 1;
+let c = "";
+let d = "Hola";
+let e = null;
+console.log(Boolean(a),
+Boolean(b),
+Boolean(c),
+Boolean(d),
+Boolean(e));
+
+/* Escribe una condición que evalúe una variable booleana y muestre un mensaje distinto en consola según si su valor es true o false.
+*/
+console.log("*******************ejercicio 3 de booleans*******************");
+let edad = 19;
+if (edad >= 18){
+    console.log(`Es verdadero`);
+} else {
+    console.log(`Es falso`);
+}
