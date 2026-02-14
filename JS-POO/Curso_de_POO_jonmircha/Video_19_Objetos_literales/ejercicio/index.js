234e1aa61886db599be9b652092cc411579dfffc
Jonathan04102006 <jonaperez0406@gmail.com>
2026-02-14 01:34:22 -0600

    ejercicio 20 de POO

--- JS-POO/Curso_de_POO_jonmircha/Video_19_Objetos_literales/ejercicio/index.js
+++ JS-POO/Curso_de_POO_jonmircha/Video_19_Objetos_literales/ejercicio/index.js
@@ -11,13 +11,43 @@ const persona = {
     saludar(){
         console.log(`Hola soy ${this.nombre} y tengo ${this.edad} años y mi pais natal es ${pais}.`)
     }
-}
+};
 
 console.log(persona);
 persona.saludar();
 
 /* Crea un objeto literal llamado auto usando propiedad abreviada (shorthand) con las variables marca, modelo y anio, y agrega un método encender que muestre un mensaje en consola. */
 console.log(`**********************ejercicio 2 de Objetos literales**********************`);
+let marca = "Toyota",
+    modelo = "Mirai",
+    anio = 2014;
+
+const auto = {
+    marca,
+    modelo,
+    anio,
+    encender(){
+        console.log(`El carro esta encendido`)
+    }
+};
+
+console.log(auto);
+auto.encender();
 
 /* Crea un objeto literal llamado producto con las propiedades nombre, precio y cantidad, y un método que calcule el total a pagar multiplicando el precio por la cantidad. */
 console.log(`**********************ejercicio 3 de Objetos literales**********************`);
+let nombre1 = "leche",
+    precio = 2.90,
+    cantidad = 5;
+
+const producto = {
+    nombre1,
+    precio,
+    cantidad,
+    calcule(){
+        console.log(`El total a pagar es: $${precio*cantidad}`)
+    }
+};
+
+console.log(producto);
+producto.calcule();
