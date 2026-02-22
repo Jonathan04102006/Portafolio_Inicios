d7390c001fb6712059ea7e916429aa6b53031318
Jonathan04102006 <jonaperez0406@gmail.com>
2026-02-21 19:58:33 -0600

    Clase 4 de TypeScript

---
+++ TypeScript/Curso_de_TypeScript_Amin/Video_4_any,_unknown,_never,_void/Variables_diferentes/index.js
@@ -0,0 +1,7 @@
+var variableAny;
+variableAny = "Hello";
+console.log("Valor de variable ".concat(variableAny));
+variableAny = 42;
+console.log("Valor de variable ".concat(variableAny));
+variableAny = true;
+console.log("Valor de variable ".concat(variableAny));
---
+++ TypeScript/Curso_de_TypeScript_Amin/Video_4_any,_unknown,_never,_void/Variables_diferentes/index.ts
@@ -0,0 +1,10 @@
+let variableAny: any;
+
+variableAny = `Hello`;
+console.log(`Valor de variable ${variableAny}`);
+
+variableAny = 42;
+console.log(`Valor de variable ${variableAny}`);
+
+variableAny = true;
+console.log(`Valor de variable ${variableAny}`);
