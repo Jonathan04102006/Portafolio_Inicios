cfa0c451c68a2152c52593435a0a88281b005b10
Jonathan04102006 <jonaperez0406@gmail.com>
2026-02-22 02:40:31 -0600

    Video 5 de Amin-Platzi

--- TypeScript/Curso_de_TypeScript_Amin/Video_4_any,_unknown,_never,_void/Variables_diferentes/index.js
+++ TypeScript/Curso_de_TypeScript_Amin/Video_4_any,_unknown,_never,_void/Variables_diferentes/index.js
@@ -5,3 +5,19 @@ variableAny = 42;
 console.log("Valor de variable ".concat(variableAny));
 variableAny = true;
 console.log("Valor de variable ".concat(variableAny));
+var variableDesconocida;
+variableDesconocida = "Hola";
+// variableDesconocida = 42;
+// variableDesconocida = true;
+if (typeof variableDesconocida === "string") {
+    console.log("Variable unknown is a string: ".concat(variableDesconocida));
+}
+/* function throwError(message : string): never {
+    throw new Error(message);
+}
+
+throwError(`Esto es un error`); */
+function logMessage(message) {
+    console.log("Log message ".concat(message));
+}
+logMessage("Es es un mensaje de log");
--- TypeScript/Curso_de_TypeScript_Amin/Video_4_any,_unknown,_never,_void/Variables_diferentes/index.ts
+++ TypeScript/Curso_de_TypeScript_Amin/Video_4_any,_unknown,_never,_void/Variables_diferentes/index.ts
@@ -8,3 +8,25 @@ console.log(`Valor de variable ${variableAny}`);
 
 variableAny = true;
 console.log(`Valor de variable ${variableAny}`);
+
+let variableDesconocida: unknown;
+
+variableDesconocida = `Hola`;
+// variableDesconocida = 42;
+// variableDesconocida = true;
+
+if (typeof variableDesconocida === `string`){
+    console.log(`Variable unknown is a string: ${variableDesconocida}`);
+}
+
+/* function throwError(message : string): never {
+    throw new Error(message);
+}
+
+throwError(`Esto es un error`); */
+
+function logMessage(message : string): void {
+    console.log(`Log message ${message}`);
+}
+
+logMessage(`Es es un mensaje de log`);
