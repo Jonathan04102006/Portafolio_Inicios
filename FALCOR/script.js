b327e61f84fdbfb48534a7e018412b3e16a746c2
Jonathan04102006 <jonaperez0406@gmail.com>
2026-02-26 17:54:28 -0600

    Logo

--- FALCOR/script.js
+++ FALCOR/script.js
@@ -4,8 +4,14 @@ function validarFormulario(){
     let password = document.querySelector(".password").value.trim();
     let error = document.querySelector(".error-message");
 
-    if(usuario === "" || password === ""){
-        error.textContent = "Usuario y Contraseña son obligatorios.";
+    if(usuario === "" & password === ""){
+        error.textContent = "Usuario y Contraseña son campos obligatorios.";
+        return false;
+    }else if(password === ""){
+        error.textContent = "Contraseña campo obligatorio.";
+        return false;
+    }else if(usuario === ""){
+        error.textContent = "Usuario campo obligatorio.";
         return false;
     }
 
