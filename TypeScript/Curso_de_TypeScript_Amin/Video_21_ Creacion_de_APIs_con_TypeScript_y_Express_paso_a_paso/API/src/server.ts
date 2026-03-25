77b3366fac343c522858e89e34a74bbd8fa8aa84
Jonathan04102006 <jonaperez0406@gmail.com>
2026-03-25 04:48:34 -0600

    TS

--- TypeScript/Curso_de_TypeScript_Amin/Video_21_ Creacion_de_APIs_con_TypeScript_y_Express_paso_a_paso/API/src/server.ts
+++ TypeScript/Curso_de_TypeScript_Amin/Video_21_ Creacion_de_APIs_con_TypeScript_y_Express_paso_a_paso/API/src/server.ts
@@ -5,16 +5,20 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-
 app.use(express.json());
 
+app.get('/hello', (req, res) => {
+  res.send('¡Hola desde tu API!');
+});
+
 app.get('/', (req, res) => {
-  res.send('Servidor API funcionando');
-});   
+  res.send('Servidor API funcionando. Visita /hello para una respuesta.');
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});   
+});
