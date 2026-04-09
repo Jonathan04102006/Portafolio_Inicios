221d4d137621f909abfe0f6fd52fd120413dc1e4
Jonathan04102006 <jonaperez0406@gmail.com>
2026-04-08 18:26:34 -0600

    JS Clase y ejercicios (34)

--- Fundamentos_de_JavaScript_Oscar_Barajas/4-Modulos y Asincronía/Vieo_34_ Async_await_vs_promesas_en_JavaScript/src/18-async.js
+++ Fundamentos_de_JavaScript_Oscar_Barajas/4-Modulos y Asincronía/Vieo_34_ Async_await_vs_promesas_en_JavaScript/src/18-async.js
@@ -1,27 +1,3 @@
-// Promise
-
-function esperar (ms) {
-    return new Promise((resolver) => setTimeout(resolver, ms));
-}
-
-function obtenerUsuario () {
-    return esperar(200).then(() => ({ id: 1, nombre: 'Ariel' }));
-}
-
-function obtenerNotas (userId) {
-    return esperar(200).then(() => ['Nota 1','Nota 2']);
-}
-
-function procesarNotas (notas) {
-    return esperar(200).then(() => notas.map((n) => n.toUpperCase()));
-}
-
-obtenerUsuario()
-    .then((usuario) => obtenerNotas(usuario.id))
-    .then((notas) => procesarNotas(notas))
-    .then((resultado) => console.log('Resultado: ', resultado))
-    .catch((error) => console.error('Error en algún paso: ', error.message));
-
 // Async/await
 
 /* async function obtenerUsuario () {
@@ -29,6 +5,35 @@ obtenerUsuario()
     return {...}
 } */
 
-function esperar () {
-    
+function esperar (ms) {
+    return new Promise((resolver) => setTimeout(resolver, ms));
 }
+
+async function obtenerUsuario () {
+    await esperar(200);
+    return {id: 1, nombre: 'Lion' };
+}
+
+async function obtenerNotas (userId) {
+    await esperar(200);
+    return ['nota 1', 'nota 2'];
+}
+
+async function procesarNotas (notas) {
+    await esperar(200);
+    return notas.map((n) => n.toUpperCase());
+}
+
+async function cargarDatos () {
+    try {
+        const usuario = await obtenerUsuario();
+        const notas = await obtenerNotas(usuario.id);
+        const resultado = await procesarNotas(notas);
+        console.log('Usuario: ', usuario.id);
+        console.log('Resultado: ', resultado);
+    } catch (error) {
+        console.error('Error', error.message);
+    }
+}
+
+cargarDatos();
