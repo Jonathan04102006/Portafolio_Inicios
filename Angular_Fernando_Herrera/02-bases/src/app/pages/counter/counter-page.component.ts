80f936089cdf9aa5943a6bb597d472e69659b250
Jonathan04102006 <jonaperez0406@gmail.com>
2026-04-16 00:27:25 -0600

    Angular-Fenando

--- Angular_Fernando_Herrera/02-bases/src/app/pages/counter/counter-page.component.ts
+++ Angular_Fernando_Herrera/02-bases/src/app/pages/counter/counter-page.component.ts
@@ -1,17 +1,32 @@
-import { Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
 
 @Component({
-    templateUrl: './counter-page.component.hmtl'
+    templateUrl: './counter-page.component.html',
+    styles: `
+        button {
+            padding: 5px;
+            margin: 5px 10px;
+            width: 75px;
+        }
+    `,
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CounterPageComponent {
     counter = 10;
+    counterSignal = signal(10);
+
+
+    constructor() {}
 
     increaseBy(value: number) {
         this.counter += value;
+        // this.counterSignal.set(this.counterSignal() + value);
+        this.counterSignal.update((current) => current + value);
     }
 
 
     resetCounter() {
-        this.counter;
+        this.counter = 0;
+        this.counterSignal.set(0);
     }
 }
---
+++ Angular_Fernando_Herrera/02-bases/src/app/pages/hero/hero-page.component.ts
@@ -0,0 +1,26 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: './hero-page.component.html',
+})
+export class HeroPageComponent {
+    name() {
+        throw new Error('Method not implement.');
+    }
+
+    age() {
+        throw new Error('Method not implement.');
+    }
+
+    getHeroDescription() {
+        throw new Error('Method not implement.');
+    }
+
+    changeHero() {
+        throw new Error('Method not implement.');
+    }
+
+    resetForm(){
+        throw new Error('Method not implement.');
+    }
+}
