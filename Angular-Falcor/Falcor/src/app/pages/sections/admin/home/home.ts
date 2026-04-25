e8940ef95cc84d04feac63f31ffd28bf2f24e0e3
Jonathan04102006 <jonaperez0406@gmail.com>
2026-04-24 23:19:44 -0600

    Inputs-Login en home

--- Angular-Falcor/Falcor/src/app/pages/sections/admin/home/home.ts
+++ Angular-Falcor/Falcor/src/app/pages/sections/admin/home/home.ts
@@ -1,9 +1,52 @@
+import { CommonModule, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [
+    FormsModule,
+    CommonModule,
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  password: string = '';
+  passwordError: string = '';
+
+  validatePassword(): void {
+    /**
+     * Reglas:
+     * - Mínimo 6 caracteres
+     * - Al menos 1 mayúscula
+     * - Al menos 1 minúscula
+     * - Al menos 1 número
+     * - Solo caracteres especiales permitidos: $ @ . *
+     */
+
+    if (!this.password) {
+      this.passwordError = '';
+      return;
+    }
+
+    // Primero validamos longitud mínima
+    if (this.password.length < 6) {
+      this.passwordError =
+        'La contraseña debe tener al menos 6 caracteres.';
+      return;
+    }
+
+    // Regex completa con mínimo 6 caracteres
+    const passwordRegex =
+      /^(?=.[A-Z])(?=.[a-z])(?=.\d)[A-Za-z\d$@.]{6,}$/;
+
+    if (!passwordRegex.test(this.password)) {
+      this.passwordError =
+        'Formato no válido. Debe contener mayúscula, minúscula, número y solo estos caracteres especiales: $ @ . *';
+    } else {
+      this.passwordError = '';
+    }
+  }
+}
--- Angular-Falcor/Falcor/src/app/pages/sections/admin/login/login.ts
+++ Angular-Falcor/Falcor/src/app/pages/sections/admin/login/login.ts
@@ -31,7 +31,7 @@ export class Login {
 
   onSubmit() {
     if (this.loginForm.valid) {
-      this.router.navigate(['/home']);   
+      this.router.navigate(['/home']);
     }
   }
 }
