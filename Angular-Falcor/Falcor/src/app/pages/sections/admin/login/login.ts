cafaa1e1bb5a848d68cc227f7c8e1c08584b8bbf
Jonathan04102006 <jonaperez0406@gmail.com>
2026-04-28 18:38:02 -0600

    Inputs mascaras

--- Angular-Falcor/Falcor/src/app/pages/sections/admin/login/login.ts
+++ Angular-Falcor/Falcor/src/app/pages/sections/admin/login/login.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [
     ReactiveFormsModule,
-    RouterLink
+    RouterLink,
+    CommonModule
   ],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
 export class Login {
   loginForm: FormGroup;
@@ -24,9 +27,32 @@ export class Login {
       password: ['', [
         Validators.required,
         Validators.minLength(8),
+        // Minimo 8 carac, 1 Mayuscula, 1 Minusculas, 1 Numemeros, 1 Especiales
         Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/)
       ]]
     });
+  }
+
+  getUsuarioError(): string {
+    const control = this.loginForm.get('usuario');
+    if (control?.touched && control.errors) {
+      if (control.errors['required']) return 'El usuario es obligatorio.';
+      if (control.errors['minlength']) return 'Debe tener al menos 6 caracteres.';
+      if (control.errors['pattern']) return 'Solo letras, números y puntos permitidos.';
+    }
+    return '';
+  }
+
+  getPasswordError(): string {
+    const control = this.loginForm.get('password');
+    if (control?.touched && control.errors) {
+      if (control.errors['required']) return 'La contraseña es obligatoria.';
+      if (control.errors['minlength']) return 'Debe tener al menos 8 caracteres.';
+      if (control.errors['pattern']) {
+        return 'Debe incluir mayúscula, minúscula, número y un carácter especial (!@#$%^&*).';
+      }
+    }
+    return '';
   }
 
   onSubmit() {
