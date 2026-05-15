27532237f74dda56126b21131f2fa84db5cd01d7
Jonathan04102006 <jonaperez0406@gmail.com>
2026-05-14 20:48:48 -0600

    TMDB

--- Angular-Tesis/TMDB/src/app/app.routes.ts
+++ Angular-Tesis/TMDB/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { Search } from './features/search/search';
+import { Favorites } from './features/favorites/favorites';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home,
+    },
+    {
+        path: 'search',
+        component: Search,
+    },
+    {
+        path: 'favorites',
+        component: Favorites,
+    },
+];
--- Angular-Tesis/TMDB/src/app/app.ts
+++ Angular-Tesis/TMDB/src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { Home } from './features/home/home';
+import { Favorites } from './features/favorites/favorites';
+import { Search } from './features/search/search';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    Home,
+    Search,
+    Favorites,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('TMDB');
+  protected readonly title = signal('TheMovieDB');
 }
